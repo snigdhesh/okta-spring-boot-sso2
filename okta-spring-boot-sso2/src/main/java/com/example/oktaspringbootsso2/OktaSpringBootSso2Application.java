1abff2171382fdae0322b8cef7f1b08b504a01fb
snigdhesh <snigdhesh@live.com>
2024-05-08 20:42:37 -0400

    Base project

---
+++ okta-spring-boot-sso2/src/main/java/com/example/oktaspringbootsso2/OktaSpringBootSso2Application.java
@@ -0,0 +1,13 @@
+package com.example.oktaspringbootsso2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaSpringBootSso2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OktaSpringBootSso2Application.class, args);
+    }
+
+}
---
+++ okta-spring-boot-sso2/src/test/java/com/example/oktaspringbootsso2/OktaSpringBootSso2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oktaspringbootsso2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaSpringBootSso2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
