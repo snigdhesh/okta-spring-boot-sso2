fdfe3479244c25ec983e5599575c31e45812b546
snigdhesh <snigdhesh@live.com>
2024-05-09 09:39:32 -0400

    Add custom css and getUserInfo from okta

--- okta-spring-boot-sso2/src/main/java/com/example/oktaspringbootsso2/controllers/UserController.java
+++ okta-spring-boot-sso2/src/main/java/com/example/oktaspringbootsso2/controllers/UserController.java
@@ -9,11 +9,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -28,5 +31,10 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @GetMapping("/profile")
+    public ModelAndView userDetails(OAuth2AuthenticationToken authentication) {
+        return new ModelAndView("profile" , Collections.singletonMap("details", authentication.getPrincipal().getAttributes()));
     }
 }
