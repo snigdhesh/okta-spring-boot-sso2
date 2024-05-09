9764b168e6310ae40d3c8102ce474dc8af66b507
snigdhesh <snigdhesh@live.com>
2024-05-09 00:07:13 -0400

    Implement okta logout

--- okta-spring-boot-sso2/src/main/java/com/example/oktaspringbootsso2/config/security/SecurityConfig.java
+++ okta-spring-boot-sso2/src/main/java/com/example/oktaspringbootsso2/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.oktaspringbootsso2.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,11 +11,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${okta.oauth2.logoutURL}")
+    private String logoutUrl;
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
 
         //Authenticate all/any requests
         httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+        //Handle logout
+        httpSecurity.logout(logout-> logout.logoutSuccessHandler(new OktaLogoutHandler(logoutUrl)));
 
         return httpSecurity.build();
     }
